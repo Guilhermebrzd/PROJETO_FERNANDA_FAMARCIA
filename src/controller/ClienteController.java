818a7b1be693a6c0db6f59004cb5ef350e1f10e8
João Antônio <tontjoao@gmail.com>
2026-05-30 09:34:01 -0300

    agora vai se deus quiser ClienteController.java

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,35 +1,36 @@
 package controller;
 
 import model.Cliente;
+import model.repository.ClienteRepository;
 import service.ClienteService;
 import java.util.List;
 
 public class ClienteController {
     private ClienteService service;
 
-    public ClienteController() {
-        this.service = new ClienteService();
+    public ClienteController(ClienteRepository repository) {
+        this.service = new ClienteService(repository);
     }
 
     public void adicionarCliente(String nome, String cpf, String telefone, String email) {
         try {
             Cliente novo = new Cliente(null, nome, cpf, telefone, email);
             service.cadastrarCliente(novo);
-            System.out.println("Cliente cadastrado com sucesso.");
+            System.out.println("Cliente cadastrado com sucesso!");
         } catch (IllegalArgumentException e) {
-            System.err.println("Erro ao cadastrar cliente: " + e.getMessage());
+            System.err.println("Erro: " + e.getMessage());
         }
     }
 
     public void exibirClientes() {
+        System.out.println("\n--- LISTA DE CLIENTES ---");
         List<Cliente> lista = service.listarClientes();
-        System.out.println("--- Lista de Clientes ---");
         if (lista.isEmpty()) {
             System.out.println("Nenhum cliente cadastrado.");
             return;
         }
         for (Cliente c : lista) {
-            System.out.println("ID: " + c.getId() + " | Nome: " + c.getNome() + " | CPF: " + c.getCpf());
+            System.out.println("ID: " + c.getId() + " | Nome: " + c.getNome() + " | CPF: " + c.getCpf() + " | Tel: " + c.getTelefone());
         }
     }
 }
